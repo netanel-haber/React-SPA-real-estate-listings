2cd7d8fa0e0b407888703dd954bdc41c87ff330d
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-01-28 11:43:12 +0200

    fixed bug - previous n_mods installed with wsl were version 64. The aws env required 72. updated nodejs on ubuntu then reinstalled modules. Rezipped and reuploaded to aws. now works.

--- aws_lambda_pic_upload/index.js
+++ aws_lambda_pic_upload/index.js
@@ -1,11 +1,11 @@
 const detect = require('./detect');
-const util = require('./util');
+const util = require('util');
 
 exports.handler = async (event) => {
     console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
     // var srcBucket = event.Records[0].s3.bucket.name;
     // // Object key may have spaces or unicode non-ASCII characters.
-    // var srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+    // var srcKey    = decodeURICompoent(event.Records[0].s3.object.key.replace(/\+/g, " "));
     // var dstBucket = srcBucket + "-resized";
     // var dstKey    = "resized-" + srcKey;
 };
