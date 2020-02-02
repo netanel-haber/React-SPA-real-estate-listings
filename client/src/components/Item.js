7e84390d97341721aaad104e78bb8de1242a83b0
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-02-02 18:54:30 +0200

    starting to work on Item comp

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,8 +1,57 @@
 import React from 'react';
+import '../styles/components/Item.scss';
 
-const Item = (props) => {
-    return <div>klklklkl</div>
+
+let types = [];
+
+
+let mockProps = {
+    listingMetaData: {
+        listerId: 34759837489,
+        listingId: 239029304,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        deletedAt: null
+        // type: 
+    },
+    coreAttr: { price: 2500 },
+    topLevelAttr: {},
+
+    subLevel: [2, 6],
+    lastLevel: [4]
 };
+
+
+
+
+class Item extends React.Component {
+    // constructor(props) {
+    //     // super(props);
+    //     // // const { isOpen = false, data } = props;
+    //     // // const { type, listerId, listingId, coreAttr, topLevelAttr, fineGrainAttr } = data;
+    //     // // const { price } = coreAttr;
+    //     // this.state = {
+    //     //     // isOpen
+    //     // }
+    // }
+
+    toggleSize() {
+        this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+    }
+
+
+    render() {
+        let el = Object.keys(mockProps).map(key => {
+            return (<div>{mockProps[key]}</div>)
+        });
+        return (
+            <div className="testing" onClick={this.toggleSize.bind(this)}>
+                
+            </div>);
+    }
+}
+
+
 
 
 
--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import Item from '../Item';
 const HomePage = (props) => (
     <div>
         HomePage
+        <Item test="here"/>
     </div>
 );
 
