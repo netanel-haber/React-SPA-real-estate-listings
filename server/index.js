f848f9d52b9d9ba89f149ea14372617f7a6c6f43
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-02-25 14:15:28 +0200

    customizing prod env for heroku

--- server/index.js
+++ server/index.js
@@ -1,25 +1,22 @@
-require('dotenv').config();
+if (!process.env.NODE_ENV) {
+    require('dotenv').config();
+}
+
 const c = require('chalk');
 const express = require('express');
 const app = express();
-
-
 app.use(require('./logger'));
 app.use(require('body-parser').json());
 app.use('/api', require('./routers/api'));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(__dirname + '/client/build'));
+}
 app.use('/icons', express.static(__dirname + '/icons'));
 
 
 
-const { PORT } = process.env;
+const { PORT = 3000 } = process.env;
 app.listen(PORT, () => {
     console.log(c.bgGreen.gray(`The server is running on port ${PORT}`));
 });
 
-
-
-
-
-
-// prod:
-// app.use(express.static('build'));
