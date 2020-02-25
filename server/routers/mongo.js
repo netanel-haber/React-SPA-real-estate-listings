d642cd34092b231e82aabb7e905d06ce503aacee
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-02-25 17:22:49 +0200

    specific express path first

--- server/routers/mongo.js
+++ server/routers/mongo.js
@@ -9,14 +9,16 @@ mongoRouter.post('/listings/count/:type', async (req, res) => {
     res.json(await countDocs(req.params.type, req.filters));
 })
 
-mongoRouter.use('/listings/:type', async (req, res) => {
-    res.json(await getTopLevel(req.params.type, req.filters, req.options));
-})
-
 
 mongoRouter.get('/listings/:type/:id', async (req, res) => {
     res.json(await getBottomLevel(req.params.type, req.params.id));
 })
+
+
+mongoRouter.use('/listings/:type', async (req, res) => {
+    res.json(await getTopLevel(req.params.type, req.filters, req.options));
+})
+
 
 mongoRouter.get('/listers/:id', async (req, res) => {
     res.json(await getLister(req.params.id));
