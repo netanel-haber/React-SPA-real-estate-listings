9988a5c8d2c5149512a2ce1f8b12054808fd4fd6
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-03-03 18:05:44 +0200

    working on cascading options.
    problems with sort dispatch.

--- client/src/components/ListContainer/ListContainer.js
+++ client/src/components/ListContainer/ListContainer.js
@@ -1,47 +1,36 @@
-import React, { useEffect, useReducer, useState, useContext } from 'react';
-import OptionsContext from '../../contexts/OptionsContext';
+import React, { useContext, useEffect, useState } from 'react';
 import ItemListContext from '../../contexts/ItemListContext';
+import OptionsContext from '../../contexts/OptionsContext';
 import { countDocs, getListings } from '../../fetch/data';
 import { ItemListWithLoader } from './ItemList';
 import Paging from './Paging';
 import SortBy from './SortBy/SortBy';
 
-const limit = 3;
-const optionsReducer = (prevState, { type, payload }) => {
-    switch (type) {
-        case "UPDATE_SORTS":
-            return { ...prevState, skip: 0, sorts: payload }
-        case "UPDATE_FILTERS":
-            return { ...prevState, skip: 0, filters: payload }
-        case "UPDATE_SKIP":
-            return { ...prevState, skip: (payload - 1) * limit }
-        default:
-            return prevState;
-    }
-}
-
 const ListContainer = (props) => {
     const { type, initialFilter } = props;
-    const { state } = useContext(OptionsContext);
-    const mergedState = { ...state, filters: { ...state.filters, ...initialFilter } }
+    const { state, dispatchFilters, dispatchSorts } = useContext(OptionsContext);
+    const [skip, dispatchSkip] = useState(0);
+    const mergedOptions = { ...state, filters: { ...state.filters, ...initialFilter }, skip }
 
     const [list, updateList] = useState([]);
     const [listUpdating, toggleUpdating] = useState(true);
     const [count, updateCount] = useState(0);
 
-    const [optionState, dispatchOptions] = useReducer(optionsReducer, {
-        sorts: { updatedAt: -1 },
-        filters: initialFilter,
-        skip: 0,
-        limit
-    });
+    console.log(mergedOptions);
 
     useEffect(() => {
         toggleUpdating(true);
-        getListings(type, optionState)
+        getListings(type, mergedOptions)
             .then(updateList).then(() => { toggleUpdating(false) })
-        countDocs(type, optionState.filters).then(updateCount);
-    }, [type, optionState])
+        dispatchSkip(0);
+        countDocs(type, mergedOptions.filters).then(updateCount);
+    }, [state.filters])
+
+    useEffect(() => {
+        toggleUpdating(true);
+        getListings(type, mergedOptions)
+            .then(updateList).then(() => { toggleUpdating(false) })
+    }, [skip])
 
     return (
         <ItemListContext.Provider value={{
@@ -49,11 +38,11 @@ const ListContainer = (props) => {
             list,
             type,
             listUpdating,
-            numberOfPages: Math.ceil(count / limit),
-            page: optionState.skip / limit + 1,
-            dispatchSorts: (payload) => { dispatchOptions({ type: "UPDATE_SORTS", payload }) },
-            dispatchFilters: (payload) => { dispatchOptions({ type: "UPDATE_FILTERS", payload }) },
-            dispatchSkip: (payload) => { dispatchOptions({ type: "UPDATE_SKIP", payload }) }
+            numberOfPages: Math.ceil(count / mergedOptions.limit),
+            page: mergedOptions.skip / mergedOptions.limit + 1,
+            dispatchSorts,
+            dispatchFilters,
+            dispatchSkip: (skipBy) => { dispatchSkip((skipBy - 1) * mergedOptions.limit) }
         }}>
             <SortBy />
             <ItemListWithLoader loading={listUpdating} />
--- client/src/components/ListsContainer.js
+++ client/src/components/ListsContainer.js
@@ -9,17 +9,16 @@ const { HEB_MITIGATED_LISTINGS } = {
 }
 
 
-
 const ListsContainer = ({ type }) => {
     const [state, dispatch] = useState({
         sorts: { updatedAt: -1 },
         filters: {},
         limit: 3
     });
-    const dispatchSort = (sorts) => { dispatch({ ...state, sorts }) };
+    const dispatchSorts = (sorts) => { dispatch({ ...state, sorts }) };
     const dispatchFilters = (filters) => { dispatch({ ...state, filters: { ...state.filters, ...filters } }) }
     return (
-        <OptionsContext.Provider value={state, dispatchSort, dispatchFilters}>
+        <OptionsContext.Provider value={{state, dispatchSorts, dispatchFilters}}>
             <div className="ItemList__container">
                 <ListContainer className="ItemList" type={type} initialFilter={{ attributeIsNull: 'listing.mitigatingCompany' }} />
                 <h5>{HEB_MITIGATED_LISTINGS}</h5>
