43ac557101723bc9980f5241ea7642d98e2a2807
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-03-09 14:19:20 +0200

    reupdated config to alias preact.

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,9 +2,9 @@
 module.exports = function override(config) {
     config.resolve.alias = {
         ...config.resolve.alias,
-        // "react": "preact/compat",
-        // "react-dom": "preact/compat",
-        // "react-dom/test-utils": "preact/test-utils"
+        "react": "preact/compat",
+        "react-dom": "preact/compat",
+        "react-dom/test-utils": "preact/test-utils"
     }
     return config;
 }
