f41d93d3d161b29a1431949413df92197b41b344
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-03-09 16:58:37 +0200

    clean up function.

--- server/middleware/serveStaticallyCompressedInstead.js
+++ server/middleware/serveStaticallyCompressedInstead.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const clientPath = path.join(__dirname, '../../client', 'build');
+const fs = require('fs');
 
 module.exports = (req, res, next) => {
     if (/\.js$/.test(req.originalUrl) && req.headers["accept-encoding"].includes("gzip")) {
-        console.log("here");
-        res.set("Content-Encoding", "gzip");
-        return res.sendFile(path.join(clientPath, req.originalUrl + '.gz'));
+        const gzippedPath = path.join(clientPath, req.originalUrl + '.gz');
+        if (fs.existsSync(gzippedPath)) {
+            res.set("Content-Encoding", "gzip");
+            return res.sendFile(gzippedPath);
+        }
     }
     next();
 };
