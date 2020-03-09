6a812c8d057f9d326f08d1c27ea2dcdec4802a62
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-03-09 23:21:14 +0200

    errors working as wanted in form.

--- client/src/components/forms/Signup.js
+++ client/src/components/forms/Signup.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useRef } from 'react';
+import { FormContext, useForm } from 'react-hook-form';
 import '../../styles/components/forms/Signup.scss';
 import { emailValidConfig, passwordValidConfig, reEnterValidConfig } from './Signup_utilities';
 import WithDivAndLabel from './WithDivAndLabel';
@@ -13,28 +13,30 @@ const { HEB_EMAIL, HEB_SEND, HEB_PASSWORD, HEB_REENTER_PASSWORD } = {
 
 
 const SignupForm = () => {
-    const { register, handleSubmit, errors, watch } = useForm()
+    const { register, handleSubmit, errors, watch, formState: { submitCount } } = useForm()
     const onSubmit = data => console.log(data);
     const curPassRef = useRef({});
     curPassRef.current = watch("password", "");
     return (
-        <div>
-            < form className="Signup__form" onSubmit={handleSubmit(onSubmit)} >
-                <WithDivAndLabel text={HEB_EMAIL} error={errors?.email?.message}>
-                    < input className="pure-input-rounded" name="email" ref={register(emailValidConfig)} />
-                </WithDivAndLabel>
-                <WithDivAndLabel text={HEB_PASSWORD} error={errors?.password?.message}>
-                    < input type="password" className="pure-input-rounded" autoComplete="off" name="password" ref={register(passwordValidConfig)} />
-                </WithDivAndLabel>
-                <WithDivAndLabel text={HEB_REENTER_PASSWORD} error={errors?.reEnterPassword?.message}>
-                    < input type="password" className="pure-input-rounded" autoComplete="off" name="reEnterPassword"
-                        ref={register(reEnterValidConfig(curPassRef))} />
-                </WithDivAndLabel>
-                <div className="Signup__submit pure-control-group">
-                    <button className="pure-button pure-button-primary" type="submit" ref={register}>{HEB_SEND}</button>
-                </div>
-            </form >
-        </div>
+        <FormContext {...{ submitCount }}>
+            <div>
+                < form className="Signup__form" onSubmit={handleSubmit(onSubmit)} >
+                    <WithDivAndLabel text={HEB_EMAIL} error={errors?.email?.message}>
+                        < input className="pure-input-rounded" name="email" ref={register(emailValidConfig)} />
+                    </WithDivAndLabel>
+                    <WithDivAndLabel text={HEB_PASSWORD} error={errors?.password?.message}>
+                        < input type="password" className="pure-input-rounded" autoComplete="off" name="password" ref={register(passwordValidConfig)} />
+                    </WithDivAndLabel>
+                    <WithDivAndLabel text={HEB_REENTER_PASSWORD} error={errors?.reEnterPassword?.message}>
+                        < input type="password" className="pure-input-rounded" autoComplete="off" name="reEnterPassword"
+                            ref={register(reEnterValidConfig(curPassRef))} />
+                    </WithDivAndLabel>
+                    <div className="Signup__submit pure-control-group">
+                        <button className="pure-button pure-button-primary" type="submit" ref={register}>{HEB_SEND}</button>
+                    </div>
+                </form >
+            </div>
+        </FormContext>
     )
 }
 
--- client/src/components/forms/WithErrorMessageContainer.js
+++ client/src/components/forms/WithErrorMessageContainer.js
@@ -1,16 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
+import { useFormContext } from 'react-hook-form';
 const WithErrorMessageContainer = ({ children: [el, error] }) => {
     const [visibility, toggleVisi] = useState("visible");
+    const { submitCount } = useFormContext();
+
     useEffect(() => {
         toggleVisi("visible");
         setTimeout(() => { toggleVisi("hidden") }, 2000)
-    }, [error, el])
+    }, [error, submitCount])
     return (
         <div className="error-message-container">
             {el}
             {error &&
-                <div className="error-message" style={{ visibility }} onMouseOver={() => { toggleVisi("hidden") }}>
+                <div className="error-message" style={{ visibility }} >
                     {error}
                 </div>
             }
