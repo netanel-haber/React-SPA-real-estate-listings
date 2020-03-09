56be1f4d4d8433d46e7b4597e50bfc3f38b47ab5
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-03-09 16:44:53 +0200

    fixed clientPath (moved from other dir)

--- server/middleware/serveStaticallyCompressedInstead.js
+++ server/middleware/serveStaticallyCompressedInstead.js
@@ -1,8 +1,9 @@
 const path = require('path');
-const clientPath = path.join(__dirname, '../client', 'build');
+const clientPath = path.join(__dirname, '../../client', 'build');
 
 module.exports = (req, res, next) => {
     if (/\.js$/.test(req.originalUrl) && req.headers["accept-encoding"].includes("gzip")) {
+        console.log("here");
         res.set("Content-Encoding", "gzip");
         return res.sendFile(path.join(clientPath, req.originalUrl + '.gz'));
     }
