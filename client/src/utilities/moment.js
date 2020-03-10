d2d52136ef7a1d7367ad70448abe0c119b2cdfb2
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-03-10 14:44:06 +0200

    disabled moment

--- client/src/utilities/moment.js
+++ client/src/utilities/moment.js
@@ -1,12 +1,12 @@
-import moment from 'moment';
-import 'moment/locale/en-il';
+// import moment from 'moment';
+// import 'moment/locale/en-il';
 
 
 const isToday = (d) => {
-    return !Boolean(moment().diff(moment(d), 'days'));
+    // return !Boolean(moment().diff(moment(d), 'days'));
 }
 const format = (d) => {
-    return moment(d).format('L');
+    // return moment(d).format('L');
 }
 
 export { isToday, format };
