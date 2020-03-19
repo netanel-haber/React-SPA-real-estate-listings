4347e632bcb74b18f6f6445ce850ede1977ebe8e
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-03-19 20:35:57 +0200

    finished styling red prompt.

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -7,7 +7,6 @@ const HomePage = (props) => (
             <div>
                 <img style={{ display: "block", maxWidth: "100%", height: "auto" }} alt="דף הבית" src="//assets.yad2.co.il/yad2site/y2assets/images/header/yad2Logo.png" />
             </div>
-
         </div>
     </div>
 );
--- client/src/hooks/useRedirect.js
+++ client/src/hooks/useRedirect.js
@@ -1,14 +1,13 @@
+import "#src#/styles/components/RedirectPrompt.scss";
 import { useHistory } from 'react-router-dom';
 import React, { useEffect } from 'react';
-import SkyLightStateless from 'react-skylight/lib/skylightstateless';
 import { paths } from '../components/pages/paths';
 
-const { HEB_NO_AUTH, HEB_LOGIN, HEB_SIGNUP, HEB_HOMEPAGE, HEB_TITLE } = {
+const { HEB_NO_AUTH, HEB_LOGIN, HEB_SIGNUP, HEB_HOMEPAGE } = {
     HEB_NO_AUTH: "אינך רשאי לגשת לדף זה משום שאינך מחובר לחשבונך האישי.",
     HEB_LOGIN: "התחבר",
     HEB_SIGNUP: "הרשם",
     HEB_HOMEPAGE: "דף הבית",
-    HEB_TITLE: "אינך מחובר לחשבונך האישי."
 }
 
 const pathEntries = [[paths.login, HEB_LOGIN], [paths.signup, HEB_SIGNUP], ['/', HEB_HOMEPAGE]]
@@ -17,8 +16,22 @@ const pathEntries = [[paths.login, HEB_LOGIN], [paths.signup, HEB_SIGNUP], ['/',
 export default function useRedirect(failedAuth) {
     const history = useHistory();
     return (
-        <SkyLightStateless onCloseClicked={() => { history.push('/') }} hideOnOverlayClicked className="f" isVisible={failedAuth} title={HEB_TITLE}>
-            {pathEntries.map(([path, text], index) => <button key={index} onClick={() => { history.push(path) }}>{text}</button>)}
-        </SkyLightStateless>
+        <div className="RedirectPrompt" style={{ display: failedAuth ? "flex" : "hidden" }} onClick={() => { history.push('/') }}>
+            <div onClick={(e) => { e.stopPropagation() }} className="content-container">
+                <div>
+                    <div className="title">{HEB_NO_AUTH}</div>
+                    <div className="RedirectPrompt__link-group pure-g">
+                        {pathEntries.map(([path, text], index) => (
+                            <div
+                                className="RedirectPrompt__link pure-u-1 pure-u-md-1-3"
+                                key={index}
+                                onClick={() => { history.push(path) }}>{text}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
+
