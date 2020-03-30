a4d8db63a40de971605d9c464861baceda8defea
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-03-30 10:04:06 +0300

    content type should be set correctly?

--- server/middleware/serveStaticallyCompressedInstead.js
+++ server/middleware/serveStaticallyCompressedInstead.js
@@ -6,8 +6,9 @@ module.exports = (req, res, next) => {
     if (/\.(js)|(css)$/i.test(req.originalUrl) && req.headers["accept-encoding"].includes("gzip")) {
         const gzippedPath = path.join(clientPath, req.originalUrl + '.gz');
         if (fs.existsSync(gzippedPath)) {
+            req.url = gzippedPath;
             res.set("Content-Encoding", "gzip");
-            return res.sendFile(gzippedPath);
+            // return res.sendFile(gzippedPath);
         }
     }
     next();
