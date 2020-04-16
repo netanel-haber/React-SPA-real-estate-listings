6e0594e23e0118c44e6e821de56858febab54575
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-04-16 15:56:02 +0300

    solved preact focus bug in DayPickerInput.
    bug only surfaced in prod where Preact was being used.
    could not uncover bug source.
    bug caused overlay to close on every input focus blur.
    it seems like an onblur event is registered on input when using preact.
    using react didnt show said onblur event.
    bug solve included overwriting lib onblur event.
    thus mimicking wanted behavior.

--- client/src/components/DayPicker.js
+++ client/src/components/DayPicker.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { dayPickerProps, format, israeliDateFormat } from '../utilities/datetime';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import '#src#/styles/components/DayPicker.scss';
 
 const { HEB_TODAY = "כניסה מיידית" } = {};
 
-const DayPicker = ({ onDayChange = () => { }, todayButton = HEB_TODAY }) => (
-    <DayPickerInput
+
+const DayPicker = ({ onDayChange = () => { }, todayButton = HEB_TODAY }) => {
+    const dp = useRef();
+    return (<DayPickerInput
+        ref={dp}
+        component={props => <input
+            {...props}
+            onBlur={(e) => !e.relatedTarget?.className?.includes("DayPicker") && dp.current.hideDayPicker() }
+        />}
         onDayChange={onDayChange}
         placeholder={israeliDateFormat}
         formatDate={format}
-        dayPickerProps={{ ...dayPickerProps, todayButton }} />
-)
+        dayPickerProps={{ ...dayPickerProps, todayButton }} />)
+}
 
 export default DayPicker;
